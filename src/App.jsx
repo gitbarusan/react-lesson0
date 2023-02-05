c33568fd700f963af1a636b355b6d5a8adfd03f7
gitbarusan <101911557+gitbarusan@users.noreply.github.com>
2023-02-06 06:58:42 +0900

    useStateについて

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 import ColorfulMessage2 from "./components/ColorfulMessage2";
 
 //jsの中でreturnしてhtmlを書いていく記法をjsx記法という
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
 
+  // useState[第一引数：stateの変数名,第二引数：stateを更新していく関数名]
+  // useState(0)←初期値を0と設定
+  // コンポーネント内で動的に変わる部分についてuseStateで定義する
+  const [num, setNum] = useState(0);
   return (
     //jsxでは1つのタグで囲わなければいけない
     //複数のタグを追加したくて親タグが不必要な場合React.Fragment
@@ -18,9 +22,10 @@ const App = () => {
       <ColorfulMessage color="blue" message="お元気ですか？" />
       <ColorfulMessage color="pink" message="元気です！" />
       <ColorfulMessage2 color="orange">あなたはどうですか？</ColorfulMessage2>
+      <p>{num}</p>
       {/* htmlのスタイルやイベントはキャメルケースケース
       ｛｝で囲うとjsとして認識される */}
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
     </>
   );
 };
