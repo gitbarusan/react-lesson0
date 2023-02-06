008e39f43e18955aea5661772461463737bea741
gitbarusan <101911557+gitbarusan@users.noreply.github.com>
2023-02-07 06:52:02 +0900

    react-lesson1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
-import ColorfulMessage2 from "./components/ColorfulMessage2";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import {
+  ColorfulMessage,
+  ColorfulMessage2
+} from "./components/ColorfulMessage";
+// import ColorfulMessage2 from "./components/ColorfulMessage2";
 
 //jsの中でreturnしてhtmlを書いていく記法をjsx記法という
 const App = () => {
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-
   // useState[第一引数：stateの変数名,第二引数：stateを更新していく関数名]
   // useState(0)←初期値を0と設定
   // コンポーネント内で動的に変わる部分についてuseStateで定義する
   const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlg = () => {
+    // !faceShowFlgで逆になる（true->false)
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  // numの値（カウントアップの数値）が３の倍数の場合faceShowFlgをtrueする
+  // 下記コードを実行するとToo many re-renders.エラーが出る
+  // stateの変更が頻発して再レンダリングのループが起こる為
+  // if (num % 3 === 0) {
+  //   setFaceShowFlg(true);
+  // } else {
+  //   setFaceShowFlg(false);
+  // }
+
+  // useEffectの第２引数は配列
+  // 第２引数を設定しないと普通の関数と変わらない（再レンダリングの度に実行される）
+  // 第２引数を空の配列にすると初回のみ実行される
+  useEffect(() => {
+    //Too many re-renders.が起きないように修正
+    // setFaceShowFlgが元々trueの場合setFaceShowFlgの関数を呼ばない
+    // || 左辺がfalseの場合右辺を実行
+    if (num % 3 === 0) {
+      faceShowFlg || setFaceShowFlg(true);
+    } else {
+      // && 左辺がtrueの場合の右辺を実行
+      faceShowFlg && setFaceShowFlg(false);
+    }
+  }, [num]);
+
   return (
     //jsxでは1つのタグで囲わなければいけない
     //複数のタグを追加したくて親タグが不必要な場合React.Fragment
@@ -22,10 +57,14 @@ const App = () => {
       <ColorfulMessage color="blue" message="お元気ですか？" />
       <ColorfulMessage color="pink" message="元気です！" />
       <ColorfulMessage2 color="orange">あなたはどうですか？</ColorfulMessage2>
-      <p>{num}</p>
       {/* htmlのスタイルやイベントはキャメルケースケース
       ｛｝で囲うとjsとして認識される */}
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlg}>on/off</button>
+      <p>{num}</p>
+      {/* faceShowFlgの戻りがtrueの場合（左辺がtrueの場合）&&の右側を返す（実行される */}
+      {faceShowFlg && <p>( ﾟДﾟ)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const contentStyle = {
     color: props.color,
     fontSize: "18px"
@@ -8,4 +8,24 @@ const ColorfulMessage = (props) => {
   return <p style={contentStyle}>{props.message}</p>;
 };
 
-export default ColorfulMessage;
+export const ColorfulMessage2 = (props) => {
+  // 分割代入
+  const { color, children } = props;
+  const contentStyle = {
+    // color: color,
+    // objectでプロパティ名と値の名前が同じの場合省略出来る
+    color,
+    fontSize: "18px"
+  };
+  return <p style={contentStyle}>{children}</p>;
+};
+// export default ColorfulMessage;
+/**
+ * export default ColorfulMessage;
+ * と書いた場合importする側は
+ * import ColorfulMessage from "./components/ColorfulMessage";
+ * export const ColorfulMessageとした場合
+ * importする側は
+ * import { ColorfulMessage } from "./components/ColorfulMessage";
+ * export default　１つの関数しかimport出来ないが、export constの書き方だと複数出来る
+ */
